99aad01080b40b9d62215dca1bc9b77b0cf78128
Taisei Nakajima <ne300085@senshu-u.jp>
2020-05-31 00:40:08 +0900

    prettier

--- src/extension.js
+++ src/extension.js
@@ -5,11 +5,12 @@ const { homedir } = require("os")
 const Twitter = require("twitter")
 
 const writeSerializedBlobToFile = (serializeBlob, fileName) => {
-  const bytes = new Uint8Array(serializeBlob.split(','))
+  const bytes = new Uint8Array(serializeBlob.split(","))
   fs.writeFileSync(fileName, Buffer.from(bytes))
 }
 
-const checkIfKeyIsNoSet = (key) => {return key === undefined || key === ''}
+const P_TITLE = "Polacode-Twitter"
+
 const credentialArray = [
   vscode.workspace.getConfiguration().get("twitter.consumerKey"),
   vscode.workspace.getConfiguration().get("twitter.consumerSecret"),
@@ -17,53 +18,42 @@ const credentialArray = [
   vscode.workspace.getConfiguration().get("twitter.accessTokenSecret"),
 ]
 
-const P_TITLE = 'Polacode 📸'
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const htmlPath = path.resolve(context.extensionPath, 'webview/index.html')
+  const htmlPath = path.resolve(context.extensionPath, "webview/index.html")
 
-  let lastUsedImageUri = vscode.Uri.file(path.resolve(homedir(), 'Desktop/code.png'))
+  let lastUsedImageUri = vscode.Uri.file(
+    path.resolve(homedir(), "Desktop/code.png")
+  )
   let panel
 
   checkIfKeyIsNoSet(credentialArray)
 
-  if (credentialArray.every(checkIfKeyIsNoSet)) {
-    vscode.window.showErrorMessage(
-      `Polacode-Twitter / Your Twitter credentials don't seem to have been entered. Please follow the extension's README to set it.`
-    )
-  }
-
-  const client = new Twitter({
-    consumer_key: credentialArray[0],
-    consumer_secret: credentialArray[1],
-    access_token_key: credentialArray[2],
-    access_token_secret: credentialArray[3]
-  })
-
-  vscode.window.registerWebviewPanelSerializer('polacode', {
+  vscode.window.registerWebviewPanelSerializer("polacode", {
     async deserializeWebviewPanel(_panel, state) {
       panel = _panel
       panel.webview.html = getHtmlContent(htmlPath)
       panel.webview.postMessage({
-        type: 'restore',
+        type: "restore",
         innerHTML: state.innerHTML,
-        bgColor: context.globalState.get('polacode.bgColor', '#2e3440')
+        bgColor: context.globalState.get("polacode.bgColor", "#2e3440"),
       })
       const selectionListener = setupSelectionSync()
       panel.onDidDispose(() => {
         selectionListener.dispose()
       })
       setupMessageListeners()
-    }
+    },
   })
 
-  vscode.commands.registerCommand('polacode.activate', () => {
-    panel = vscode.window.createWebviewPanel('polacode', P_TITLE, 2, {
+  vscode.commands.registerCommand("polacode.activate", () => {
+    panel = vscode.window.createWebviewPanel("polacode", P_TITLE, 2, {
       enableScripts: true,
-      localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'webview'))]
+      localResourceRoots: [
+        vscode.Uri.file(path.join(context.extensionPath, "webview")),
+      ],
     })
 
     panel.webview.html = getHtmlContent(htmlPath)
@@ -75,19 +65,22 @@ function activate(context) {
 
     setupMessageListeners()
 
-    const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
-    const bgColor = context.globalState.get('polacode.bgColor', '#2e3440')
+    const fontFamily = vscode.workspace.getConfiguration("editor").fontFamily
+    const bgColor = context.globalState.get("polacode.bgColor", "#2e3440")
     panel.webview.postMessage({
-      type: 'init',
+      type: "init",
       fontFamily,
-      bgColor
+      bgColor,
     })
 
     syncSettings()
   })
 
-  vscode.workspace.onDidChangeConfiguration(e => {
-    if (e.affectsConfiguration('polacode') || e.affectsConfiguration('editor')) {
+  vscode.workspace.onDidChangeConfiguration((e) => {
+    if (
+      e.affectsConfiguration("polacode") ||
+      e.affectsConfiguration("editor")
+    ) {
       syncSettings()
     }
   })
@@ -95,33 +88,33 @@ function activate(context) {
   function setupMessageListeners() {
     panel.webview.onDidReceiveMessage(({ type, data }) => {
       switch (type) {
-        case 'shoot':
+        case "shoot":
           vscode.window
             .showSaveDialog({
               defaultUri: lastUsedImageUri,
               filters: {
-                Images: ['png']
-              }
+                Images: ["png"],
+              },
             })
-            .then(uri => {
+            .then((uri) => {
               if (uri) {
                 writeSerializedBlobToFile(data.serializedBlob, uri.fsPath)
                 lastUsedImageUri = uri
               }
             })
           break
-        case 'getAndUpdateCacheAndSettings':
+        case "getAndUpdateCacheAndSettings":
           panel.webview.postMessage({
-            type: 'restoreBgColor',
-            bgColor: context.globalState.get('polacode.bgColor', '#2e3440')
+            type: "restoreBgColor",
+            bgColor: context.globalState.get("polacode.bgColor", "#2e3440"),
           })
 
           syncSettings()
           break
-        case 'updateBgColor':
-          context.globalState.update('polacode.bgColor', data.bgColor)
+        case "updateBgColor":
+          context.globalState.update("polacode.bgColor", data.bgColor)
           break
-        case 'invalidPasteContent':
+        case "invalidPasteContent":
           vscode.window.showInformationMessage(
             "Pasted content is invalid. Only copy from VS Code and check if your shortcuts for copy/paste have conflicts."
           )
@@ -157,24 +150,26 @@ function activate(context) {
   }
 
   function syncSettings() {
-    const settings = vscode.workspace.getConfiguration('polacode')
-    const editorSettings = vscode.workspace.getConfiguration('editor', null)
+    const settings = vscode.workspace.getConfiguration("polacode")
+    const editorSettings = vscode.workspace.getConfiguration("editor", null)
     panel.webview.postMessage({
-      type: 'updateSettings',
-      shadow: settings.get('shadow'),
-      transparentBackground: settings.get('transparentBackground'),
-      backgroundColor: settings.get('backgroundColor'),
-      target: settings.get('target'),
-      ligature: editorSettings.get('fontLigatures')
+      type: "updateSettings",
+      shadow: settings.get("shadow"),
+      transparentBackground: settings.get("transparentBackground"),
+      backgroundColor: settings.get("backgroundColor"),
+      target: settings.get("target"),
+      ligature: editorSettings.get("fontLigatures"),
     })
   }
 
   function setupSelectionSync() {
-    return vscode.window.onDidChangeTextEditorSelection(e => {
+    return vscode.window.onDidChangeTextEditorSelection((e) => {
       if (e.selections[0] && !e.selections[0].isEmpty) {
-        vscode.commands.executeCommand('editor.action.clipboardCopyWithSyntaxHighlightingAction')
+        vscode.commands.executeCommand(
+          "editor.action.clipboardCopyWithSyntaxHighlightingAction"
+        )
         panel.postMessage({
-          type: 'update'
+          type: "update",
         })
       }
     })
@@ -182,9 +177,9 @@ function activate(context) {
 }
 
 function getHtmlContent(htmlPath) {
-  const htmlContent = fs.readFileSync(htmlPath, 'utf-8')
+  const htmlContent = fs.readFileSync(htmlPath, "utf-8")
   return htmlContent.replace(/script src="([^"]*)"/g, (match, src) => {
-    const realSource = 'vscode-resource:' + path.resolve(htmlPath, '..', src)
+    const realSource = "vscode-resource:" + path.resolve(htmlPath, "..", src)
     return `script src="${realSource}"`
   })
 }
